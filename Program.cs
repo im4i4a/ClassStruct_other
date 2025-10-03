0a87807627a95445c394d59d33d4c1033e353318
im4i4a <al.zemskov03@yandex.ru>
2025-10-03 21:41:08 +0300

    Выполнил мини задание

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@
         static void Main(string[] args)
         {
 
-
             Human human = new Human();
 
             human.Greetings();
@@ -16,76 +15,111 @@
 
             Pen pen = new Pen();
             pen.Output();
-            pen = new Pen("Red",300);
+            pen = new Pen("Red", 300);
             pen.Output();
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.Square();
+            rectangle = new Rectangle(5);
+            rectangle.Square();
+            rectangle = new Rectangle(12, 7);
+            rectangle.Square();
         }
     }
 
-    class Human
+    class Rectangle
     {
-        // Поля класса
-        public string name;
-        public int age;
+        public int a;
+        public int b;
 
-        // Метод класса
-        public void Greetings() => Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
-        
+        public void Square()
+        {
+            Console.WriteLine($"Площадь равна {a * b}");
+        }
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
 
-        // Конструктор 1
-        public Human()
-        {
-            name = "Неизвестно";
-            age = 20;
         }
-        // Конструктор 2
-        public Human(string n)
+        public Rectangle(int spechial)
         {
-            name = n;
-            age = 20;
-        }
-        // Конструктор 3
-        public Human(string n, int a)
-        {
-            name = n;
-            age = a;
-        }
-    }
+            a = spechial;
+            b = spechial;
 
-    class Pen
-    {
-        public string color;
-        public int cost;
-
-        public void Output()
-        {
-            Console.WriteLine($"{color}  {cost}");
         }
-        public Pen()
+        public Rectangle(int one, int two)
         {
-            color = "Черный";
-            cost = 100;
-        }
-
-        public Pen(string penColor, int penCost)
-        {
-            color= penColor;
-            cost = penCost;
-        }
-    }
-    struct Animal
-    {
-        public string name;
-        public int age;
-        public string type;
-        public void Greetings()
-        {
-            Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
-        }
-
-        public Animal()
-        {
-            Console.WriteLine($"{name}   {age}   {type}");
+            a=one;
+            b=two;
 
         }
     }
-}
+
+
+        class Human
+        {
+            // Поля класса
+            public string name;
+            public int age;
+
+            // Метод класса
+            public void Greetings() => Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+
+
+            // Конструктор 1
+            public Human()
+            {
+                name = "Неизвестно";
+                age = 20;
+            }
+            // Конструктор 2
+            public Human(string n)
+            {
+                name = n;
+                age = 20;
+            }
+            // Конструктор 3
+            public Human(string n, int a)
+            {
+                name = n;
+                age = a;
+            }
+        }
+
+        class Pen
+        {
+            public string color;
+            public int cost;
+
+            public void Output()
+            {
+                Console.WriteLine($"{color}  {cost}");
+            }
+            public Pen()
+            {
+                color = "Черный";
+                cost = 100;
+            }
+
+            public Pen(string penColor, int penCost)
+            {
+                color = penColor;
+                cost = penCost;
+            }
+        }
+        struct Animal
+        {
+            public string name;
+            public int age;
+            public string type;
+            public void Greetings()
+            {
+                Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+            }
+
+            public Animal() => Console.WriteLine($"{name}   {age}   {type}");
+
+
+        }
+    }
